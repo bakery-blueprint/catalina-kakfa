3ce4e51641e21a61a0ca16c4adbf0a27c6b372ea
ssookie <hyunsue77@gmail.com>
2022-05-26 20:37:02 +0900

    카프카 실습 03 "카프카 클라이언트 - 프로듀서"

---
+++ chapter3/simple-kafka-producer/src/test/java/com/example/simplekafkaproducer/ProducerWithKeyValueTests.java
@@ -0,0 +1,34 @@
+package com.example.simplekafkaproducer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * 메시지 키가 포함된 레코드를 전송한다.
+ */
+public class ProducerWithKeyValueTests {
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
+        producer.send(record);
+        ProducerRecord<String, String> record2 = new ProducerRecord<>(TOPIC_NAME, "Busan", "Busan");
+        producer.send(record2);
+
+        producer.flush();
+        producer.close();
+    }
+}
